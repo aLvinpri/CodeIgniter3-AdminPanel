148be0ea81f4e0dcd5eb71de615aeba8dab4ea0c
aLvinpri <alvhient@gmail.com>
2019-08-28 11:45:18 +0700

    vid 3 done

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -19,10 +19,10 @@ class Auth extends CI_Controller
     // $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
     // if ($this->form_validation->run() == false) {
-        $data['title'] = 'Login Page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+    $data['title'] = 'Login Page';
+    $this->load->view('templates/auth_header', $data);
+    $this->load->view('auth/login');
+    $this->load->view('templates/auth_footer');
     // } else {
     //     // validasinya success
     //     $this->_login();
@@ -70,53 +70,53 @@ class Auth extends CI_Controller
 
   public function registration()
   {
-  //     if ($this->session->userdata('email')) {
-  //         redirect('user');
-  //     }
+    //     if ($this->session->userdata('email')) {
+    //         redirect('user');
+    //     }
 
-  //     $this->form_validation->set_rules('name', 'Name', 'required|trim');
-  //     $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-  //         'is_unique' => 'This email has already registered!'
-  //     ]);
-  //     $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
-  //         'matches' => 'Password dont match!',
-  //         'min_length' => 'Password too short!'
-  //     ]);
-  //     $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+    $this->form_validation->set_rules('name', 'Name', 'required|trim');
+    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+      'is_unique' => 'This email has already registered!'
+    ]);
+    $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+      'matches' => 'Password dont match!',
+      'min_length' => 'Password too short!'
+    ]);
+    $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
-  //     if ($this->form_validation->run() == false) {
-          $data['title'] = 'User Registration';
-          $this->load->view('templates/auth_header', $data);
-          $this->load->view('auth/registration');
-          $this->load->view('templates/auth_footer');
-  //     } else {
-  //         $email = $this->input->post('email', true);
-  //         $data = [
-  //             'name' => htmlspecialchars($this->input->post('name', true)),
-  //             'email' => htmlspecialchars($email),
-  //             'image' => 'default.jpg',
-  //             'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-  //             'role_id' => 2,
-  //             'is_active' => 0,
-  //             'date_created' => time()
-  //         ];
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'User Registration';
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/registration');
+      $this->load->view('templates/auth_footer');
+    } else {
+      $email = $this->input->post('email', true);
+      $data = [
+        'name' => htmlspecialchars($this->input->post('name', true)),
+        'email' => htmlspecialchars($email),
+        'image' => 'default.jpg',
+        'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+        'role_id' => 2,
+        'is_active' => 0,
+        'date_created' => time()
+      ];
 
-  //         // siapkan token
-  //         $token = base64_encode(random_bytes(32));
-  //         $user_token = [
-  //             'email' => $email,
-  //             'token' => $token,
-  //             'date_created' => time()
-  //         ];
+      //         // siapkan token
+      //         $token = base64_encode(random_bytes(32));
+      //         $user_token = [
+      //             'email' => $email,
+      //             'token' => $token,
+      //             'date_created' => time()
+      //         ];
 
-  //         $this->db->insert('user', $data);
-  //         $this->db->insert('user_token', $user_token);
+      $this->db->insert('user', $data);
+      //         $this->db->insert('user_token', $user_token);
 
-  //         $this->_sendEmail($token, 'verify');
+      //         $this->_sendEmail($token, 'verify');
 
-  //         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
-  //         redirect('auth');
-  //     }
+      $this->session->set_flashdata('authmessage', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
+      redirect('auth');
+    }
   }
 
 
